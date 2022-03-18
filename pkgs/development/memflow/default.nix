0a44b59df1f47fe0240ccf8cd45e69c4ca663c93
weewoo22 <91314050+weewoo22@users.noreply.github.com>
2022-03-18 18:32:53 -0400

    nix flake update

--- pkgs/development/memflow/default.nix
+++ pkgs/development/memflow/default.nix
@@ -15,7 +15,7 @@ pkgs.rustPlatform.buildRustPackage rec {
   doCheck = false;
 
   # Compile memflow with the lib/ path of each connector plugin
-  MEMFLOW_EXTRA_PLUGIN_PATHS =
+  MEMFLOW_PLUGIN_PATHS =
     lib.concatStringsSep ":"
       (builtins.map
         (connector: "${connector}/lib/") # Turn each connector plugin package output into a lib/ path
